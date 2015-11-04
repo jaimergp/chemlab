5086f47c96dd2c75549d82acef44d68d4cca7da8
Jaime RG <jaime.rogue@gmail.com>
2015-11-04 17:04:55 +0100

    Ball and Stick renderer should admit color_scheme. Fixed.

--- chemlab/graphics/renderers/ballandstick.py
+++ chemlab/graphics/renderers/ballandstick.py
@@ -1,18 +1,20 @@
 from .base import AbstractRenderer
 from .atom import AtomRenderer
 from .bond import BondRenderer
-
+from .. import colors
 from ...db import ChemlabDB
 cdb = ChemlabDB()
 
+
 class BallAndStickRenderer(AbstractRenderer):
+
     '''Render a ball and stick representation of a series of
     coordinates and bonds.
-    
+
     .. image:: /_static/ballandstick_renderer.png
-    
+
     **Parameters**
-    
+
     widget:
         The parent QChemlabWidget
     r_array: np.ndarray((NATOMS, 3), dtype=float)
@@ -25,33 +27,35 @@ class BallAndStickRenderer(AbstractRenderer):
 
 
     '''
-    def __init__(self, widget, r_array, type_array, bonds, shading='phong'):
+
+    def __init__(self, widget, r_array, type_array, bonds, shading='phong',
+                 color_scheme=colors.default_atom_map):
         super(BallAndStickRenderer, self).__init__(widget)
-        vdw_dict = cdb.get("data", 'vdwdict')        
-        
+        vdw_dict = cdb.get("data", 'vdwdict')
+
         scale = 0.3
         for k in vdw_dict:
             vdw_dict[k] = vdw_dict[k] * scale
-        
+
         self.has_bonds = len(bonds) > 0
-        
+
         self.ar = AtomRenderer(widget, r_array, type_array,
-                               radii_map = vdw_dict, shading=shading)
-        
+                               radii_map=vdw_dict, shading=shading,
+                               color_scheme=color_scheme)
+
         if self.has_bonds:
             self.br = BondRenderer(widget, bonds, r_array, type_array, style='impostors',
                                    shading=shading)
 
-        
     def draw(self):
         self.ar.draw()
-        
+
         if self.has_bonds:
             self.br.draw()
 
     def update_positions(self, r_array):
         '''Update the coordinate array r_array'''
         self.ar.update_positions(r_array)
-        
+
         if self.has_bonds:
             self.br.update_positions(r_array)
